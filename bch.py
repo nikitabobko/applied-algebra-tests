fa929a55949308ec01bd78e7cbe797c4d364a416
Nikolay Korolev <korolevns98@gmail.com>
2019-05-12 14:13:19 +0300

    Add initial bch.py and gf.py

---
+++ bch.py
@@ -0,0 +1,12 @@
+class BCH:
+    def __init__(self, n, t):
+        pass
+
+    def encode(self, U):
+        pass
+
+    def decode(self, W, method='euclid'):
+        pass
+
+    def dist(self):
+        pass
